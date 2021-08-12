0a572a5833e0c69b012d63dc326edd4ee2aea542
swetha sri <swetha.sri@sirahu.com>
2021-08-12 17:07:35 +0530

    form validation

--- ngapp/src/app/app.module.ts
+++ ngapp/src/app/app.module.ts
@@ -2,13 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormvComponent } from './formv/formv.component';
+import { MatformComponent } from './matform/matform.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule} from '@angular/material/menu';
+import { MatIconModule} from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule} from '@angular/material/select';
+import { MatCheckboxModule} from '@angular/material/checkbox';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormvComponent,
+    MatformComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
